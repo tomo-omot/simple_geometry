8c9f29c5ad718aa773411403df3c315d60483e08
Tomo Šala <hcs.omot@gmail.com>
2017-09-08 15:09:34 +0200

    Refactor DetailsObtainer Service to use Entity
    At this point, I'm relying on Entity to take care of its own area and
    perimeter. I just prepare the output and return it.

--- src/HcsOmot/Geometry/ShapesBundle/Services/ShapeDetailsObtainer.php
+++ src/HcsOmot/Geometry/ShapesBundle/Services/ShapeDetailsObtainer.php
@@ -10,21 +10,9 @@ class ShapeDetailsObtainer
 {
     public function getShapeDetails(ShapeInterface $shape): array
     {
-        $shapeType = get_class($shape);
-        switch ($shapeType) {
-            case "HcsOmot\Geometry\ShapesBundle\Entity\Triangle":
-                $type      = 'triangle';
-                $area      = $this->calculateTriangleArea($shape);
-                $perimeter = $this->calculateTrianglePerimeter($shape);
-                break;
-            case "HcsOmot\Geometry\ShapesBundle\Entity\Circle":
-                $type      = 'circle';
-                $area      = $this->calculateCircleArea($shape);
-                $perimeter = $this->calculateCirclePerimeter($shape);
-                break;
-            default:
-                throw new \InvalidArgumentException('Currently supported types are Triangle and Circle.');
-        }
+        $type = get_class($shape);
+        $perimeter = $shape->getPerimeter();
+        $area = $shape->getArea();
 
         return [
             'type'      => $type,
@@ -33,25 +21,5 @@ class ShapeDetailsObtainer
         ];
     }
 
-    private function calculateTriangleArea(Triangle $triangle)
-    {
-        $semiperimeter = $this->calculateTrianglePerimeter($triangle) / 2;
 
-        return sqrt($semiperimeter * ($semiperimeter - $triangle->getSideA()) * ($semiperimeter - $triangle->getSideB()) * ($semiperimeter - $triangle->getSideC()));
-    }
-
-    private function calculateTrianglePerimeter(Triangle $triangle)
-    {
-        return $triangle->getSideA() + $triangle->getSideB() + $triangle->getSideC();
-    }
-
-    private function calculateCircleArea(Circle $circle)
-    {
-        return M_PI * ($circle->getRadius() ** 2);
-    }
-
-    private function calculateCirclePerimeter(Circle $circle)
-    {
-        return 2 * $circle->getRadius() * M_PI;
-    }
 }
