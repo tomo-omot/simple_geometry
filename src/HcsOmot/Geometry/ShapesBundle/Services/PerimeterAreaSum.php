570c38dba166026e9532d928038bf7a12e0208d3
Tomo Šala <hcs.omot@gmail.com>
2017-09-08 15:16:33 +0200

    Create a service for perimeter/area sum for shapes
    Created a service that calculates the sum of areas or perimeters of
    a circle and triangle (calculates a sum of areas/perimeters for two
    different shapes).
    
    I'm relying on each shape to take care of it's own behaviour.

---
+++ src/HcsOmot/Geometry/ShapesBundle/Services/PerimeterAreaSum.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace HcsOmot\Geometry\ShapesBundle\Services;
+
+use HcsOmot\Geometry\ShapesBundle\Entity\Circle;
+use HcsOmot\Geometry\ShapesBundle\Entity\Triangle;
+
+class PerimeterAreaSum
+{
+    /**
+     * @var \HcsOmot\Geometry\ShapesBundle\Entity\Triangle
+     */
+    private $triangle;
+    /**
+     * @var \HcsOmot\Geometry\ShapesBundle\Entity\Circle
+     */
+    private $circle;
+
+    private $perimeterSum;
+
+    private $areaSum;
+
+
+    public function __construct(Triangle $triangle, Circle $circle)
+    {
+        $this->triangle = $triangle;
+        $this->circle   = $circle;
+
+        $this->calculatePerimeterSum();
+        $this->calculateAreaSum();
+    }
+
+    public function getPerimeterSum()
+    {
+        return $this->perimeterSum;
+    }
+
+    public function getAreaSum()
+    {
+        return $this->areaSum;
+    }
+
+    private function calculatePerimeterSum()
+    {
+        $trianglePerimeter = $this->triangle->getPerimeter();
+        $circlePerimeter   = $this->circle->getPerimeter();
+
+        $this->perimeterSum = $trianglePerimeter + $circlePerimeter;
+    }
+
+    private function calculateAreaSum()
+    {
+        $triangleArea = $this->triangle->getArea();
+        $circleArea   = $this->circle->getArea();
+
+        $this->areaSum = $triangleArea + $circleArea;
+    }
+}
