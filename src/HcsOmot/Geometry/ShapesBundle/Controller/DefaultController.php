33e4e567e3fcc2089cfdee95e0b1e5f3a1d1799c
Tomo Šala <hcs.omot@gmail.com>
2017-09-08 11:53:45 +0200

    Create triangle and circle routes
    The /triangle and /circle GET routes will return json representation
    of triangle or circle entity data, respectively.

--- src/HcsOmot/Geometry/ShapesBundle/Controller/DefaultController.php
+++ src/HcsOmot/Geometry/ShapesBundle/Controller/DefaultController.php
@@ -2,8 +2,12 @@
 
 namespace HcsOmot\Geometry\ShapesBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use HcsOmot\Geometry\ShapesBundle\Entity\Circle;
+use HcsOmot\Geometry\ShapesBundle\Entity\Triangle;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -13,5 +17,49 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('HcsOmotGeometryShapesBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/triangle/{sideA}/{sideB}/{sideC}")
+     * @Method("GET")
+     *
+     * @param float $sideA
+     * @param float $sideB
+     * @param float $sideC
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function triangle(float $sideA, float $sideB, float $sideC)
+    {
+        $triangle = new Triangle($sideA, $sideB, $sideC);
+
+        $shapeProcessor = $this->get('hcs_omot_geometry_shapes.shape_details_obtainer');
+
+        $shapeDetails = $shapeProcessor->getShapeDetails($triangle);
+
+        $response = new JsonResponse($shapeDetails);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/circle/{radius}")
+     * @Method("GET")
+     *
+     * @param float $radius
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function circle(float $radius)
+    {
+        $circle = new Circle($radius);
+
+        $shapeProcessor = $this->get('hcs_omot_geometry_shapes.shape_details_obtainer');
+
+        $shapeDetails = $shapeProcessor->getShapeDetails($circle);
+
+        $response = new JsonResponse($shapeDetails);
+
+        return $response;
     }
 }
